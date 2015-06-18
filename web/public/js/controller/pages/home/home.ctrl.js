70938cfd959fab629ba442efc11b167beccc435f
Jason Mullins <jason.m.mullins@gmail.com>
2015-06-18 00:18:36 -0400

    finished default home

--- web/public/js/controller/pages/home/home.ctrl.js
+++ web/public/js/controller/pages/home/home.ctrl.js
@@ -1,26 +1,75 @@
 auroraApp.controller('HomeCtrl', function($scope, $location, $routeParams) {
-  var _stateTabs;
+  var _stateTabs, _allStates;
 
   var init = function () {
     var abbr;
-    _stateTabs = [];
+    buildAllStates();
+    if (!localStorage['stateTabs']) {
+      localStorage['stateTabs'] = '{}';
+    }
+    _stateTabs = JSON.parse(localStorage['stateTabs']);
+    if (!localStorage['stateTabs']) {
+      //localStorage['stateTabs'] = '{}';
+    }
     if ($routeParams.state) {
       abbr = $routeParams.state.toUpperCase();
       if (abbr != 'ADD' && !StateHash[abbr]) {
         $location.path('/');
       }
       else if (abbr != 'ADD') {
-        _stateTabs.push(StateHash[abbr]);
+        saveTabIfNotInStorage(abbr);
+        localStorage['activeTab'] = abbr;
+      }
+      else if (abbr == 'ADD' || !localStorage['activeTab']) {
+        localStorage['activeTab'] = 'ADD';
       }
     }
   };
-  init();
 
   $scope.isAdding = function () {
-    return !$routeParams.state || $routeParams.state.toUpperCase() == 'ADD';
+    if (localStorage['activeTab'] == 'ADD' || !$routeParams || !localStorage['activeTab']) {
+      return true;
+    }
+    if ($routeParams.state && $routeParams.state.toUpperCase() == 'ADD') {
+      return true;
+    }
+    return false;
   };
 
   $scope.getStateTabs = function () {
     return _stateTabs;
   };
+
+  $scope.isActive = function (tab) {
+    return tab === localStorage['activeTab'];
+  };
+
+  $scope.getStateName = function (abbr) {
+    return StateHash[abbr];
+  };
+
+  $scope.getAllStates = function () {
+    return _allStates;
+  };
+
+  var buildAllStates = function () {
+    var key;
+    _allStates = [];
+    for (key in StateHash.reverse) {
+      _allStates.push(StateHash.reverse[key]);
+    }
+  };
+
+  var tabExists = function (stateAbbr) {
+    return !!_stateTabs[stateAbbr];
+  };
+
+  var saveTabIfNotInStorage = function (stateAbbr) {
+    if (!tabExists(stateAbbr)) {
+      _stateTabs[stateAbbr] = {state: stateAbbr}; // more info (like search options) will go here
+      localStorage['stateTabs'] = JSON.stringify(_stateTabs);
+    }
+  };
+
+  init();
 });
