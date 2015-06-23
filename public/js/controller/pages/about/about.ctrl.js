ceb13e3bae9e137cc20352996877d170e934af4e
dphamsd <do.pham@mythsoftware.com>
2015-06-23 10:26:56 -0400

    Removed loadable pattern and added myself to humans.txt

--- public/js/controller/pages/about/about.ctrl.js
+++ public/js/controller/pages/about/about.ctrl.js
@@ -1,5 +1,5 @@
 auroraApp.controller('AboutCtrl', function($scope, $controller) {
-  angular.extend(this, $controller('LoadableMixin', {$scope: $scope}));
+
   	$scope.aboutmessage = 'This web application is designed to present the user with up to date data of food recalls by state.  The system will also alert users via SMS when a new recall has been detected, thereby raising awareness and safety. Additional specs/ technologies used can go here as well.'
   	$scope.aboutus = 'Some blurb about you guys and your history together.'
   		// wait for a second to simulate loading
