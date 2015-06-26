9e311ab054a9d912eb2356102f1ae0e9d09b9976
Michael Schor <michael.n.schor@gmail.com>
2015-06-26 13:07:25 -0400

    url must start with http:// or it will not work

--- functional_tests/tests/smoketests/happyPath.js
+++ functional_tests/tests/smoketests/happyPath.js
@@ -4,7 +4,7 @@ var expect = chai.expect;
 module.exports = {
     'Verify Aurora is up and Live' : function (browser) {
         browser
-            .url('aurora.mythsoftware.com/')
+            .url('http://aurora.mythsoftware.com/')
             .waitForElementVisible("#crit-navbar", 1000)
             .assert.title('Aurora')
             .assert.containsText('body', 'Track food threats near you')
