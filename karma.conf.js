a6b72728bcbc768541b438ed23746e175f472525
Greg Lambert <gregory.lambert@contractor.ca.com>
2015-07-06 22:30:19 -0500

    restoring  phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine', 'requirejs'],
+        frameworks: ['jasmine'],
 
 
         // list of files / patterns to load in the browser
@@ -62,7 +62,7 @@ module.exports = function (config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: [
-            //'Firefox',
+            'Firefox',
             //'Safari',
             //'Chrome'
             'PhantomJS'
